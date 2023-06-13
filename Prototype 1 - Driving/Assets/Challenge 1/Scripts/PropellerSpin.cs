151857af81900e064e6c3a5c92d795b7922835e8
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-12 22:44:36 -0700

    minor changes

--- Prototype 1 - Driving/Assets/Challenge 1/Scripts/PropellerSpin.cs
+++ Prototype 1 - Driving/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class PropellerSpin : MonoBehaviour
 {
-    public GameObject plane;
-    public Vector3 offset;
+    public GameObject propeller;
+    public Vector3 rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.Rotate(rotation * Time.deltaTime);
     }
 }
