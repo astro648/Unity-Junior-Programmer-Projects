b0e7bf74a334040382cb1129150637da9b0e2757
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-13 16:27:47 -0700

    spawn delay and interval variables

--- Prototype 2 - Stampede/Assets/Scripts/SpawnManager.cs
+++ Prototype 2 - Stampede/Assets/Scripts/SpawnManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -7,12 +8,14 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
