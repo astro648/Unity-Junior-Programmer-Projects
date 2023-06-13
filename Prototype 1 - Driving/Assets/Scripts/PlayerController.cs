bf5733f55018ab1adea40f7be3a8561e46e3b79c
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-12 21:45:33 -0700

    semicolon

--- Prototype 1 - Driving/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Driving/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,6 @@ public class PlayerController : MonoBehaviour
 
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime)
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
