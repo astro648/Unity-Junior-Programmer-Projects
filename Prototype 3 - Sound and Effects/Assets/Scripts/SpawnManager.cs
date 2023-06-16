59cff60d80b1aa8eafcab92e1106dd07d04d3f2e
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-16 15:30:00 -0700

    find player controller gameobject

--- Prototype 3 - Sound and Effects/Assets/Scripts/SpawnManager.cs
+++ Prototype 3 - Sound and Effects/Assets/Scripts/SpawnManager.cs
@@ -8,8 +8,11 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos =  new Vector3(25, 0,0);
     private float startDelay = 2f;
     private float repeatRate = 2f;
+    private PlayerController playerControllerScript;
+
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
