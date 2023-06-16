a637eca88f646b0415427af1c3aa287861dc4a93
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-16 16:30:07 -0700

    death animation

--- Prototype 3 - Sound and Effects/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -41,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
